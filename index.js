0a6ea8aaec4ad7ade13f13020f274d1b0b2aab5d
tomas carponi <tomas1carponi@gmail.com>
2022-09-05 22:11:02 -0300

    arrays

--- index.js
+++ index.js
@@ -215,7 +215,7 @@ for(let i = 0; i < 3; i++){
 
 console.log(array);
 */
-
+//
 /*
 class Producto{
   constructor(nombre, precio){
@@ -235,4 +235,27 @@ for(let i = 0; i < 7; i++){
 }
 
 console.log(array);
+*/
+// rederizacion de dom //
+/*
+const productos = [
+  { id: 1, nombre: "camisa", precio: 1000 },
+  { id: 2, nombre: "pantalon", precio: 700 },
+  { id: 3, nombre: "gorra", precio: 1300 },
+  { id: 4, nombre: "zapato", precio: 1500 },
+];
+
+let contenedor = document.getElementById("contenedor");
+let precio = parseInt(prompt("Ingrese el precio minimo"));
+
+let productosFiltrados = productos.filter(item => item.precio > precio);
+
+for (const producto of productosFiltrados) {
+  let div = document.createElement("div");
+  div.innerHTML = `<h3> ID: ${producto.id}</h3>
+                   <p>Producto: ${producto.nombre}</p>
+                   <b>${producto.precio}</b>`;
+
+  contenedor.append(div);
+}
 */
