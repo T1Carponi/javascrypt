7a375b184ad1f62f4ebb8583e87a800073895b4d
tomas carponi <tomas1carponi@gmail.com>
2022-09-07 22:17:41 -0300

    primer final

--- index2.js
+++ index2.js
@@ -1,22 +1,13 @@
-class anteojoSol {
-    constructor (Rayban1,precio,cantidad)[
-        this.Rayban1 = Rayban1;
-        this.precio = precio;
-        this.cantidad = cantidad;
-    ]
-articulo(){
-    this.cantidad = this.cantidad +1
-}
-
-
-}
-
-const array = [];
-
-for(let i = 0; i < 7; i++){
-  let modelo = prompt("ingrese el modelo");
-  let precio = prompt("ingrese el precio");
-  let obj = new Producto(modelo, precio);
-
-  array.push(obj);
-}
+const productos = [
+    {nombre: 'cremaCorporal', precio: 1500},
+    {nombre: 'sahumerio', precio: 800},
+    {nombre: 'perfume', precio: 5000},
+    {nombre: 'aceites', precio: 2500},
+]
+ const existe = productos.some(producto => producto.nombre === "cremaCorporal,sahumerio,perfume,aceites")
+const resultado = productos.filter((el) => el.nombre.includes(''))
+const resultado2 = productos.filter((el) => el.precio < 4000)
+ 
+console.log(existe)
+console.log(resultado)
+console.log(resultado2) 
